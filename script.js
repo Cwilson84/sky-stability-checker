0367f26905558a5a1d4b7394fa2ddbadc841aba8
Chris Wilson <chrisw@myself.com>
2023-02-08 14:28:10 -0500

    trying to get it going

--- script.js
+++ script.js
@@ -11,7 +11,11 @@ var displayCityWeather = function(data) {
     $("#city-wind").text("Wind Speed: " + windSpeed + " mph");
 };
 
-  var getCityWeather = function getWeatherData(city) {
+var display5DayForecast = function(data) {
+  // code to display the 5 day forecast data goes here
+};
+
+var getCityWeather = function getWeatherData(city) {
     var cityName = $("#search-bar").val().trim();
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=21d879b7b731a9f3ad1beaec9dc719e4&units=imperial";
     fetch(apiUrl)
@@ -30,18 +34,47 @@ var displayCityWeather = function(data) {
     });
 };
 
+var get5DayForecast = function(city) {
+  var cityName = $("#search-bar").val().trim();
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=21d879b7b731a9f3ad1beaec9dc719e4&units=imperial";
+  fetch(apiUrl)
+    .then(function(response) {
+      if (response.ok) {
+        response.json()
+          .then(function(data) {
+            display5DayForecast(data);
+          });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect to OpenWeather server. Please try again later.");
+    });
+};
+
 $("#search-button").click(function(event) {
     event.preventDefault(); // prevent the form from submitting
     var city = $("#search-bar").val().trim();
     getCityWeather(city);
+    get5DayForecast(city);
+    searchHistory.push(city);
+    localStorage.setItem("lastCitySearched", city);
 });
 
 $(document).ready(function() {
   $("#search-button").click(function(event) {
     event.preventDefault(); // prevent the form from submitting
     var city = $("#search-bar").val();
-    getCityWeather(city);
+    var lastCitySearched = localStorage.getItem("lastCitySearched");
+        if (lastCitySearched !== null) {
+            $("#search-bar").val(lastCitySearched);
+            getCityWeather(lastCitySearched);
+            get5DayForecast(lastCitySearched);
+        } else {
+            $("#search-bar").val(city);
+            getCityWeather(city);
+            get5DayForecast(city);
+        }
   });
 });
-
-  
